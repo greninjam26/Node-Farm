1b1576aebacd1e492960cde314de9382ef9bd879
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-02-15 21:39:52 -0500

    implement async reading and writting to file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // file system modules
 const fs = require("fs");
 
+// Blocking
 // read from files
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
@@ -9,3 +10,23 @@ console.log(textIn);
 const textOut = `This is what we know about hte avocado: ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File written!");
+
+// Non-Blocking
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+	if (err) return console.log("ERROR!!!");
+
+	fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+		console.log(data2);
+		fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+			console.log(data3);
+
+			// writting
+			fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", error => {
+				console.log("Written");
+			});
+		});
+		console.log("Reading Again...");
+	});
+	console.log("Still Reading...");
+});
+console.log("Reading...");
