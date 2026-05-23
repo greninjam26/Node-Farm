e1daf6419ad2845238c408c45e90f61a7a3e38ae
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-05-23 15:47:29 -0400

    implemented modules

--- index.js
+++ index.js
@@ -3,25 +3,12 @@ const fs = require("fs");
 // webserver
 const http = require("http");
 const url = require("url");
+// modules
+const replaceTemplate = require("./modules/replaceTemplate");
 
 ////////////////////////////////////////////////////
 // Server
 ////////////////////////////////////////////////////
-
-const replaceTemplate = (temp, product) => {
-	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-
-	if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-	return output;
-};
 
 // we can keep it syncrinsis, blocking the code doesn't matter in this case
 // the top level code is only run once at the start anyway
