ed8c097a26c8bbb8f77159a6aea2e34fb4826acf
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-05-23 12:18:05 -0400

    implemented html requests

--- index.js
+++ index.js
@@ -42,22 +42,62 @@ console.log("Reading...");
 // Server
 ////////////////////////////////////////////////////
 
+const replaceTemplate = (temp, product) => {
+	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRICE%}/g, product.price);
+	output = output.replace(/{%FROM%}/g, product.from);
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+
+	if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+	return output;
+};
+
 // we can keep it syncrinsis, blocking the code doesn't matter in this case
 // the top level code is only run once at the start anyway
 // __dirname, will make the path start from the folder the current script is located in
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/template-overview.html`,
+	"utf-8",
+);
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	"utf-8",
+);
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 // req: request
 // res: response
+// this will be run once at every request
 const server = http.createServer((req, res) => {
 	const pathname = req.url;
 
+	// Overview page
 	if (pathname === "/" || pathname === "/overview") {
-		res.end("This is the overview");
-	} else if (pathname == "/product") {
-		res.end("This is the product");
-	} else if (pathname == "/api") {
+		res.writeHead(200, { "content-type": "text/html" });
+
+		const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+		const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
+
+		res.end(output);
+	}
+	// Product page
+	else if (pathname == "/product") {
+		res.writeHead(200, { "content-type": "text/html" });
+
+		const productHTML = dataObj.map(el => replaceTemplate(tempProduct, el)).join("");
+
+		res.end(productHTML);
+	}
+	// API
+	else if (pathname == "/api") {
 		// this is not efficient, because everytime something go to the route, the data is fetched
 		// we should keep the fetching outside and only do it once
 		// fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (error, data) => {
@@ -67,7 +107,9 @@ const server = http.createServer((req, res) => {
 		// });
 		res.writeHead(200, { "content-type": "application/json" });
 		res.end(data);
-	} else {
+	}
+	// Not Found page
+	else {
 		res.writeHead(404, {
 			"Content-type": "text/html",
 			"my-own-header": "hello-world",
